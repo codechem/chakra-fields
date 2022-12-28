4a560e599228bbc25a5ed94c59e7ee50f244619b
dejan.slamkov <dejan@codechem.com>
2022-12-28 13:03:03 +0100

    add form control field

---
+++ src/lib/fields/form-control-field.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { FieldMetaProps } from 'formik';
+import { FormControl, FormErrorMessage, FormLabel, FormErrorMessageProps, FormControlProps } from '@chakra-ui/react';
+
+import { FieldLabelProps } from '../types';
+import { floatingLabelProps } from '../utils';
+
+export type FormControlFieldProps = {
+    meta: FieldMetaProps<any>;
+    labelProps?: FieldLabelProps;
+    errorMessageCss?: FormErrorMessageProps;
+} & FormControlProps;
+
+const FormControlField: React.FC<FormControlFieldProps> = ({ meta, labelProps, errorMessageCss, children, ...formControlProps }: FormControlFieldProps) => {
+	let formLabel = null;
+    if (labelProps?.label) {
+        formLabel = (<FormLabel {...labelProps?.labelCss}>{labelProps.label}</FormLabel>);
+        if (labelProps?.labelPosition === 'floating') {
+            formLabel = (<FormLabel {...labelProps?.labelCss} {...floatingLabelProps}>{labelProps.label}</FormLabel>);
+        }
+    }
+	return (
+		<FormControl {...formControlProps} isInvalid={!!meta.error && meta.touched}>
+			{labelProps?.labelPosition === 'before' && formLabel}
+			{children}
+			{(labelProps?.labelPosition === 'after' || labelProps?.labelPosition === 'floating') && formLabel}
+			<FormErrorMessage {...errorMessageCss}>{meta.error}</FormErrorMessage>
+		</FormControl>
+	);
+};
+
+export default FormControlField;
--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,15 +1,17 @@
-import { FormLabelProps } from '@chakra-ui/react';
+import { FormControlProps, FormErrorMessageProps, FormLabelProps } from '@chakra-ui/react';
+
+export type FieldLabelProps = {
+	label?: React.ReactNode;
+	labelPosition?: 'before' | 'after' | 'floating';
+	labelCss?: FormLabelProps;
+};
 
 export type FieldProps = {
 	name: string;
-	label?: string;
-	labelPosition?: 'before' | 'after' | 'floating';
-};
+	formControlCss?: FormControlProps;
+	errorMessageCss?: FormErrorMessageProps;
+} & FieldLabelProps;
 
 export type ValidatedFieldProps<T = any> = {
 	validate?: (value: T) => undefined | string | Promise<any>;
 } & FieldProps;
-
-export type CustomLabelProps = {
-	labelCss?: FormLabelProps;
-};
